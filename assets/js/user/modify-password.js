1c7a1c57ddbea0d06baf39242bce046a299285cf
szk <1484851205@qq.com>
2021-01-31 15:05:20 +0800

    完成了用户头像更换功能

--- assets/js/user/modify-password.js
+++ assets/js/user/modify-password.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
 
     const { form, layer } = layui
 
@@ -7,14 +7,14 @@ $(function() {
             /^\w{6,12}$/,
             '密码必须6到12位，且不能出现空格'
         ],
-        confirmPass: function(val) {
+        confirmPass: function (val) {
             if (val !== $('#pass').val()) {
                 return '两次输入的密码不一致！'
             }
         }
     })
 
-    $('.layui-form').submit(function(e) {
+    $('.layui-form').submit(function (e) {
         e.preventDefault()
 
         axios.post('/my/updatepwd', $(this).serialize())
@@ -25,6 +25,10 @@ $(function() {
                     return layer.msg('修改密码失败！')
                 }
                 layer.msg('修改密码成功！')
+                // 跳转到登录页
+                window.parent.location.href = '../login.html'
+                // 原先 taken令牌清除
+                localStorage.removeItem('token')
             })
     })
 })
